0b9e45683d870b29ace4931686f604bdbead3b6c
seancarroll <seanc28@gmail.com>
2018-09-27 08:14:52 -0500

    Position.START should use 0 for user version not Integer.MAX_VALUE. Updates Positin.END to use Integer.MAX_VALUE as well instead of hex 0xffffffff.

--- src/main/java/com/seancarroll/foundationdb/es/Position.java
+++ src/main/java/com/seancarroll/foundationdb/es/Position.java
@@ -10,21 +10,19 @@ import java.nio.ByteOrder;
  */
 public class Position {
 
-    //public static final Versionstamp START = Versionstamp.incomplete(0);
     public static final Versionstamp START = Versionstamp.fromBytes(ByteBuffer.allocate(Versionstamp.LENGTH)
         .order(ByteOrder.BIG_ENDIAN)
         .putLong(0L)
-        .putInt(0xffffffff)
+        .putInt(0)
         .array());
 
 
     // TODO: is this a valid way to do END?
     public static final Versionstamp END = Versionstamp.fromBytes(ByteBuffer.allocate(Versionstamp.LENGTH)
-                .order(ByteOrder.BIG_ENDIAN)
-                .putLong(Long.MAX_VALUE)
-                .putInt(0xffffffff)
-                .array());
-
+        .order(ByteOrder.BIG_ENDIAN)
+        .putLong(Long.MAX_VALUE)
+        .putInt(Integer.MAX_VALUE)
+        .array());
 
     private Position() {
         // public static fields only
