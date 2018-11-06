9dd3c66695a4853871ffe956fcc68d2a567d69d0
seancarroll <seanc28@gmail.com>
2018-11-05 22:23:52 -0600

    adds BenchmarkTests which just contains some commented out sample benchmark tests but will be home for some actual benchmark tests at some point in the future

---
+++ src/test/java/com/seancarroll/foundationdb/es/BenchmarkTests.java
@@ -0,0 +1,61 @@
+package com.seancarroll.foundationdb.es;
+
+// TODO: add benchmark tests
+// http://hg.openjdk.java.net/code-tools/jmh/file/tip/jmh-samples/src/main/java/org/openjdk/jmh/samples/
+// https://wiki.openjdk.java.net/display/HotSpot/PerformanceTechniques
+// https://antoniogoncalves.org/2015/01/15/micro-benchmarking-with-jmh-measure-dont-guess/
+// https://github.com/dropwizard/dropwizard/tree/master/dropwizard-benchmarks/src/main/java/io/dropwizard/benchmarks
+class BenchmarkTests {
+
+//    @Benchmark
+//    @BenchmarkMode(Mode.Throughput)
+//    @OutputTimeUnit(TimeUnit.SECONDS)
+//    public void measureThroughput() throws InterruptedException {
+//        TimeUnit.MILLISECONDS.sleep(100);
+//    }
+
+//    @Benchmark
+//    @BenchmarkMode(Mode.AverageTime)
+//    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+//    public void measureAvgTime() throws InterruptedException {
+//        TimeUnit.MILLISECONDS.sleep(100);
+//    }
+
+//    @Benchmark
+//    @BenchmarkMode(Mode.SampleTime)
+//    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+//    public void measureSamples() throws InterruptedException {
+//        TimeUnit.MILLISECONDS.sleep(100);
+//    }
+
+//    @Benchmark
+//    @BenchmarkMode(Mode.SingleShotTime)
+//    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+//    public void measureSingleShot() throws InterruptedException {
+//        TimeUnit.MILLISECONDS.sleep(100);
+//    }
+
+//    @Benchmark
+//    @BenchmarkMode({Mode.Throughput, Mode.AverageTime, Mode.SampleTime, Mode.SingleShotTime})
+//    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+//    public void measureMultiple() throws InterruptedException {
+//        TimeUnit.MILLISECONDS.sleep(100);
+//    }
+
+//    @Benchmark
+//    @BenchmarkMode(Mode.All)
+//    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+//    public void measureAll() throws InterruptedException {
+//        TimeUnit.MILLISECONDS.sleep(100);
+//    }
+
+//    public static void main(String[] args) throws RunnerException {
+//        Options opt = new OptionsBuilder()
+//            .include(BenchmarkTests.class.getSimpleName())
+//            .forks(1)
+//            .build();
+//
+//        new Runner(opt).run();
+//    }
+
+}
