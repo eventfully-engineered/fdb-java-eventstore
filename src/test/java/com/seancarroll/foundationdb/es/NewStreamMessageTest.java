2a496538f278d2c5f9fba07ec18ab9b4008bbd26
seancarroll <seanc28@gmail.com>
2018-11-25 12:39:05 -0600

    adds test to verify exception thrown if message size exceeds maximum size

---
+++ src/test/java/com/seancarroll/foundationdb/es/NewStreamMessageTest.java
@@ -0,0 +1,18 @@
+package com.seancarroll.foundationdb.es;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class NewStreamMessageTest {
+
+    @Test
+    void shouldThrowWhenMessageSizeExceedsMaximum() {
+        String data = StringUtils.repeat("a", 100_000);
+        assertThrows(IllegalArgumentException.class, () -> new NewStreamMessage(UUID.randomUUID(), "SomeType", data.getBytes(), "metadata".getBytes()));
+    }
+
+}
